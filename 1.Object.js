b05f1124bceebb2b2abb505a17d7f7d75e21b328
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-10-05 18:26:04 +0530

    Object

---
+++ 1.Object.js
@@ -0,0 +1,18 @@
+let person = {
+	first_name: 'John',
+	last_name: 'Doe',
+
+	//method
+	getPersonDetails: function() {
+		return `The name of the person is
+				${person.first_name} ${person.last_name}`
+	},
+}
+
+console.log(person.first_name);
+console.log(person.getPersonDetails());
+
+
+// John
+// The name of the person is
+// 				John Doe
